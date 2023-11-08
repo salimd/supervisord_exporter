2f220c382fa7df814d3c649f04402cddff76a518
Salim Dervisoglu <salimd@gmail.com>
2023-11-08 10:35:46 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# Supervisor Exporter
+
+The Supervisor Exporter is a simple Go application that collects process status information from the Supervisor process control system and exposes it as Prometheus metrics. This allows you to monitor the state of processes managed by Supervisor.
+
+## Table of Contents
+
+- [Features](#features)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Usage](#usage)
+- [Configuration](#configuration)
+- [Prometheus Metrics](#prometheus-metrics)
+- [License](#license)
+
+## Features
+
+- Collects process status information from Supervisor.
+- Exposes process state, exit status, and more as Prometheus metrics.
+- Configurable via command line parameters.
+- Provides a simple HTTP server for Prometheus to scrape metrics.
+- Handles unreachable Supervisord XML-RPC endpoints gracefully.
+
+## Getting Started
+
+### Prerequisites
+
+Before running the Supervisor Exporter, make sure you have the following prerequisites:
+
+- Go (Golang) installed on your system.
+
+### Installation
+
+1. Clone the repository:
+
+   ```shell
+   git clone https://github.com/salimd/supervisord_exporter.git
+   ```
+
+2. Build the application:
+   ```shell
+   go build
+   ```
+
+### Usage
+
+To start the Supervisord Exporter, run the following command:
+
+   ```shell
+   ./supervisord_exporter
+   ```
+
+By default, the exporter will listen on port 9876 and use the Supervisor XML-RPC interface at `http://localhost:9001/RPC2`. You can change the defaults using command line parameters (see [Configuration](#configuration) section).
+
+### Configuration
+
+The Supervisord Exporter can be configured using command line parameters. Here are the available parameters:
+
+* `-supervisord-url`: The URL of the Supervisord XML-RPC interface. Default is `http://localhost:9001/RPC2`
+* `-web.listen-address`: The address and port where the exporter will listen for HTTP requests. Default is `:9876`
+* `-web.telemetry-path`: Path under which to expose metrics. Default is `/metrics`
+* `-version`: Print the version information and exit.
+
+Example of custom configuration:
+
+```shell
+./supervisord_exporter -supervisord-url="http://example.com:9001/RPC2" -web.listen-address=":8080" -web.telemetry-path="/metrics"
+```
+
+### Prometheus Metrics
+
+The Supervisord Exporter exposes the following Prometheus metric:
+
+* `supervisor_process_info`: Gauge vector with labels for `name`, `group`, `state`, `start`, and `exit_status`. The `state` label will be `RUNNING` if the process is running, and `exit_status` will be `0` for a running process.
+* `supervisord_up`: Gauge metric indicating the status of the connection to Supervisord (1 if up, 0 if down). If the Supervisord XML-RPC endpoint is unreachable, this metric will be set to 0, and there will be no supervisor_process_info metrics in the output.
+
+
+Sample metric:
+```
+supervisor_process_info{exit_status="0",group="apache2",name="apache2",state="RUNNING"} 1
+supervisord_up{} 1
+```
+
+### License
+
+This project is licensed under the MIT License
+
---
+++ supervisord_exporter.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/kolo/xmlrpc"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	supervisordURL string
+	listenAddress  string
+	metricsPath    string
+	version        bool
+	appVersion     float32 = 0.1
+
+	processesMetric = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "supervisor_process_info",
+			Help: "Supervisor process information",
+		},
+		[]string{"name", "group", "state", "start", "exit_status"},
+	)
+	supervisordUp = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "supervisord_up",
+			Help: "Supervisord XML-RPC connection status (1 if up, 0 if down)",
+		},
+	)
+)
+
+func init() {
+	flag.StringVar(&supervisordURL, "supervisord-url", "http://localhost:9001/RPC2", "Supervisord XML-RPC URL")
+	flag.StringVar(&listenAddress, "web.listen-address", ":9876", "Address to listen for HTTP requests")
+	flag.StringVar(&metricsPath, "web.telemetry-path", "/metrics", "Path under which to expose metrics")
+	flag.BoolVar(&version, "version", false, "Displays application version")
+
+	flag.Parse()
+}
+
+func fetchSupervisorProcessInfo() {
+	client, err := xmlrpc.NewClient(supervisordURL, nil)
+	if err != nil {
+		log.Printf("Error creating Supervisor XML-RPC client: %v", err)
+		supervisordUp.Set(0)
+		processesMetric.Reset()
+		return
+	}
+	defer client.Close()
+
+	result := []map[string]interface{}{}
+	if err := client.Call("supervisor.getAllProcessInfo", nil, &result); err != nil {
+		log.Printf("Error calling Supervisor XML-RPC method: %v", err)
+		supervisordUp.Set(0)
+		processesMetric.Reset()
+		return
+	}
+
+	supervisordUp.Set(1)
+
+	// Create a map to store the latest process information for each unique combination of name and group
+	latestInfo := make(map[string]map[string]interface{})
+
+	for _, data := range result {
+		name, _ := data["name"].(string)
+		group, _ := data["group"].(string)
+
+		// Generate a unique key for the combination of name and group
+		key := name + group
+
+		// Check if the latest information for this combination already exists
+		if existing, ok := latestInfo[key]; ok {
+			// Compare timestamps to determine which information is more recent
+			existingStartTime, _ := existing["start"].(int64)
+			newStartTime, _ := data["start"].(int64)
+
+			// If the new information is more recent, update the latestInfo map
+			if newStartTime > existingStartTime {
+				latestInfo[key] = data
+			}
+		} else {
+			// If no previous information exists for this combination, add it to the map
+			latestInfo[key] = data
+		}
+	}
+
+	// Clear the previous metric values
+	processesMetric.Reset()
+
+	for _, data := range latestInfo {
+		name, _ := data["name"].(string)
+		group, _ := data["group"].(string)
+		state, _ := data["statename"].(string)
+		start, _ := data["start"].(int64)
+		exitStatus, _ := data["exitstatus"].(int)
+
+		value := 0
+		if state == "RUNNING" {
+			value = 1
+		}
+
+		processesMetric.WithLabelValues(name, group, state, fmt.Sprintf("%d", start), fmt.Sprintf("%d", exitStatus)).Set(float64(value))
+	}
+}
+
+func metricsHandler(w http.ResponseWriter, r *http.Request) {
+	fetchSupervisorProcessInfo()
+	promhttp.Handler().ServeHTTP(w, r)
+}
+
+func main() {
+	if version {
+		fmt.Printf("Supervisor Exporter v%v\n", appVersion)
+		os.Exit(0)
+	}
+
+	prometheus.MustRegister(processesMetric)
+	prometheus.MustRegister(supervisordUp)
+
+	http.HandleFunc(metricsPath, metricsHandler)
+
+	fmt.Printf("Listening on %s\n", listenAddress)
+	if err := http.ListenAndServe(listenAddress, nil); err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
+}
